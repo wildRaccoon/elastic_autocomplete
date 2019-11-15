8aea39d89ba149991060c5eaa8eafb246a835e01
wildRaccoon <chukvm@gmail.com>
2019-11-15 18:18:27 +0200

    add data generator

---
+++ data_generator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Bogus;
+
+namespace data_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = 1;
+            var testUsers = new Faker<UserData>()
+                .CustomInstantiator(r => new UserData() {Id = id++})
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.Email, (f, u) => f.Internet.Email(u.FirstName,u.LastName))
+                .RuleFor(p => p.Tags, (f, u) =>
+                new List<string>()
+                    {
+                        f.Address.Country(), f.Address.City(), f.Vehicle.Manufacturer()
+                    });
+            
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+            Console.WriteLine(JsonSerializer.Serialize(testUsers.Generate()));
+        }
+    }
+}
---
+++ data_generator/UserData.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace data_generator
+{
+    public class UserData
+    {
+        public string FirstName { get; set; }
+        
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        
+        public List<string> Tags { get; set; }
+    }
+}
